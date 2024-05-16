49bd154b963fb47d5187eda37dd4fe78a025eeaf
nleanba <25827850+nleanba@users.noreply.github.com>
2024-05-16 14:10:05 +0200

    deno file-server does set correct text/turtle content-type

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
 import { GHActServer } from "./deps.ts";
 import { ghActConfig } from "../config/config.ts";
 
-// TODO ensure that turtle served from /workdir/repository still has the correct Content-Type set
-// if (pathname.startsWith("/repo")) {
-//   console.log("· Got file request for", pathname);
-//   const response = await serveDir(request, {
-//     fsRoot: "workdir/repo",
-//     urlRoot: "repo",
-//   });
-//   response.headers.set("Content-Type", "text/turtle");
-//   return response;
-
 const worker = new Worker(import.meta.resolve("./worker.ts"), {
   type: "module",
 });
