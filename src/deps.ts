f4211bddfb8c4094c571d7eaf7947449b61642ff
nleanba <25827850+nleanba@users.noreply.github.com>
2024-05-26 13:40:57 +0200

    updated GHAct to deno.land version 1.2.0

--- src/deps.ts
+++ src/deps.ts
@@ -3,5 +3,4 @@ export {
   GHActServer,
   GHActWorker,
   type Job,
-} from "https://raw.githubusercontent.com/factsmission/ghact/faster-git/mod.ts";
-// TODO revert to deno.land after relevant version is published
+} from "https://deno.land/x/ghact@1.2.0/mod.ts";
