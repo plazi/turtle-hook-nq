4b2ada60584b7746ef3257fdf9030f4b9b54dbff
Reto Gmür <reto@factsmission.com>
2025-11-01 10:18:57 +0000

    removed indirection

--- src/deps.ts
+++ src/deps.ts
@@ -5,12 +5,10 @@ export {
   type Job,
 } from "https://deno.land/x/ghact@1.4.0/mod.ts";
 
-export { existsSync } from "https://deno.land/x/ghact@1.4.0/src/deps.ts";
-
 export {
   dirname,
   join,
   relative,
 } from "https://deno.land/std@0.224.0/path/mod.ts";
 
-export { walk } from "https://deno.land/std@0.224.0/fs/mod.ts";
+export { existsSync, walk } from "https://deno.land/std@0.224.0/fs/mod.ts";
